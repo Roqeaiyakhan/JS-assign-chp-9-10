26b5c8b45d10714fa9f63dfc133700863d494da7
Roqeaiyakhan <roqaiyajaani@gmail.com>
2024-05-28 16:24:17 +0500

    add js-chp-9-10

---
+++ index.js
@@ -0,0 +1,52 @@
+//var brtcardbalance = +prompt("brt cardbalance kitna ha" );
+
+//if(brtcardbalance >= 60){
+   // alert("welcome to peshawarZu")
+//}
+ 
+//if(brtcardbalance <= 30){
+   // alert("recharge your balance")
+//}
+
+
+//CHAPTER-9 PROMPTS
+//used to take user input
+
+// !...........if we take the input from user it also show in alert and console  so we write below type programe.............!
+var userAge = +prompt("Please enter your age!");
+console.log("You are " + userAge + " years old.");
+alert("You are " + userAge + " years old.");
+
+// const userName = prompt("Please enter your name");
+// alert("Helllooooo " + userName);
+// console.log("helllooo " + userName);
+
+// const firstName = prompt("Please enter your first name ");
+// const lastName = prompt("Please enter your last name ");
+// const fullName = firstName + " " + lastName;
+
+// alert("Helloooo " + fullName);
+
+// STATIONARY SHOP BILLING SYSTEM
+// const pencil = prompt("Price for pencil");
+// const notebook = prompt("price for notebook");
+// const totalBill = pencil + notebook;
+
+// alert("your total bill is " + totalBill);
+
+//prompts always return string values
+//const pencil = +prompt("Price for pencil");
+//const notebook = +prompt("price for notebook");
+//const totalBill = pencil + notebook;
+
+//alert("your total bill is " + totalBill);
+
+// var fullName = prompt;
+// var fullname = prompt;
+
+// if(fullName){
+//     alert("ayesha print kardo");
+// }
+// if(fullname){
+//     alert("javeria print karo");
+// }
